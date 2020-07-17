c8f4ea19ec411e54a47d7a28aaf4bb9def6bb068
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-17 15:29:39 -0300

    requisito 3 feedback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Settings from './pages/Settings';
 import GameScreen from './pages/GameScreen';
 import Ranking from './pages/Ranking';
+import Feedback from './pages/FeedBack';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/GameScreen" component={GameScreen} />
+        <Route path="/Feedback" component={Feedback} />
         <Route exact path="/Settings" component={Settings} />
         <Route exact path="/Ranking" component={Ranking} />
       </Switch>
--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const feedbackText = (assertions) => {
   if (assertions < 3) return 'Podia ser melhor...';
@@ -8,6 +9,9 @@ const feedbackText = (assertions) => {
 };
 
 const FeedBack = () => {
+  const { score, assertions }; //vem de algum lugar;
+
+  return (
   <div>
     <div>
       <Header />
@@ -16,8 +20,18 @@ const FeedBack = () => {
       <h1><strong>Feedback</strong></h1>
       <div>
         <h3 data-testid="feedback-text">{feedbackText(assertions)}</h3>
+        <h4>Pontuação <span data-testid="feedback-total-score">{score}</span></h4>
+        <h4>Acertos <span data-testid="feedback-total-question">{assertions}</span></h4>
       </div>
+    <Link to="/" >
+      <button data-testid="btn-play-again">Jogar novamente</button>
+    </Link>
+    <Link to={'Ranking'}>
+      <button data-testid="btn-ranking">Ver Ranking</button>
+    </Link>
     </div>
-
   </div>
+  );  
 }
+
+export default FeedBack;
