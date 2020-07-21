857245a40f256b17059347dbdaf02c9da33feea9
Joao Soutto <joaosoutto@hotmail.com>
2020-07-21 17:27:44 -0300

    teste

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -8,6 +8,7 @@ class Ranking extends React.Component {
     return (
       <div>
         <Header />
+        <h1 data-testid="ranking-title">Ranking</h1>
         <Link to="/">
           <button type="button" data-testid="btn-go-home">
             Voltar à tela inicial
